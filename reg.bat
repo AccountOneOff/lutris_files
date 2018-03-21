507e4e48062cea8dc302f590261b8f5e1ffd2c84
cxf <throwawayaccx@gmail.com>
2018-03-21 14:49:38 -0700

    Delete reg.bat

--- reg.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-start /c "C:\Windows\system32\cmd.exe"
-REG ADD "HKLM\SOFTWARE\Khronos"
-REG ADD "HKLM\SOFTWARE\Khronos\Vulkan"
-REG ADD "HKLM\SOFTWARE\Khronos\Vulkan\Drivers" /v "C:\Windows\winevulkan.json" /t REG_DWORD /d 0x00000000 /f /reg:64
-exit 0

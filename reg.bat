cce7a608e46a39f8b212fafd5439953535c498de
cxf <throwawayaccx@gmail.com>
2018-03-21 00:36:14 -0700

    Update reg.bat

--- reg.bat
+++ reg.bat
@@ -1,1 +1,2 @@
-cmd.exe /c start REG ADD "HKLM\SOFTWARE\Khronos\Vulkan\Drivers" /v "C:\Windows\winevulkan.json" /t REG_DWORD /d 0x00000000 /f /reg:64
+start cmd.exe /k REG ADD "HKLM\SOFTWARE\Khronos\Vulkan\Drivers" /v "C:\Windows\winevulkan.json" /t REG_DWORD /d 0x00000000 /f /reg:64
+exit 0
